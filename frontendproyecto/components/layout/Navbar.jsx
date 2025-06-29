1ed020ed138b59874aa9a464d5530e722c419b03
Gutierrez101 <mateoguti2021info@gmail.com>
2025-06-29 15:04:58 -0500

    mejora en el diseño

--- frontendproyecto/components/layout/Navbar.jsx
+++ frontendproyecto/components/layout/Navbar.jsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 
 export const Navbar = () => (
-  <header className="bg-gray-800 text-white p-4 flex justify-between">
-    <h1 className="font-bold">MiStudyClass</h1>
+  <header className="bg-green-600 text-white p-4 flex justify-between">
+    <h1 className="font-bold">App-Learning</h1>
     <nav className="flex gap-4">
       <Link href="/dashboard/estudiante">Estudiante</Link>
       <Link href="/dashboard/docente">Docente</Link>
