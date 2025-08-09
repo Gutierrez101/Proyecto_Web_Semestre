a8134551e136a90f17565d7a7a52b85ed1528b8e
DuvalM95 <duvalmunoz2@gmail.com>
2025-08-09 16:05:13 -0500

    V3.4 Aplicación de API OpenAI

--- backendproyecto/api/views.py
+++ backendproyecto/api/views.py
@@ -5,7 +5,7 @@ from apiAI.api import evaluar_prueba_openai
 class EvaluarPruebaIA(APIView):
     def post(self, request):
         banco_preguntas = request.data.get('preguntas', [])
-        api_key = '***REMOVED***'
+        api_key = 'API OPENAI'
         resultado = evaluar_prueba_openai(banco_preguntas, api_key)
         if resultado:
             return Response(resultado)
