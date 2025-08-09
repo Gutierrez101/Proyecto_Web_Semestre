e3aacea15adcf55e6952af647a60fa27a30c6dcb
DuvalM95 <duvalmunoz2@gmail.com>
2025-08-09 18:26:41 -0500

    Elimina archivos .pyc con secretos

--- backendproyecto/api/views.py
+++ backendproyecto/api/views.py
@@ -1,11 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from apiAI.api import evaluar_prueba_openai
+import os
 
 class EvaluarPruebaIA(APIView):
     def post(self, request):
         banco_preguntas = request.data.get('preguntas', [])
-        api_key = 'OPENAI_API_KEY'
+        api_key = os.getenv('OPENAI_API_KEY')
         resultado = evaluar_prueba_openai(banco_preguntas, api_key)
         if resultado:
             return Response(resultado)
